25c6aa03d6099db9b75b2be4a14973542656d4b0
Samuel <s.mitra@sportradar.com>
2019-12-29 21:32:54 +0100

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Samuil Mitra Blog`,
+    author: `Samuil Mitra`,
+    description: `Idei, ganduri, culegeri.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      facebook: `samuilmitra`,
     },
   },
   plugins: [
